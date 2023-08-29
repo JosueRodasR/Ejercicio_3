aaab862fcd53a9552a7e77c6c69d3e3ba108448c
Usuario <Usuario@DESKTOP-U4P8J7O>
2023-08-28 20:49:10 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float cp1, cp2, cp3, cef, ctf, nf;
+            Console.WriteLine("Ingrese su primera Calificacion Parcial: ");
+            cp1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Ingrese su segunda Calificacion Parcial; ");
+            cp2 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Ingrese su tercera Calificacion Parcial; ");
+            cp3 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Ingrese su Calificacion del Examen Final: ");
+            cef = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Ingrese su Calificacion del Trabajo Final: ");
+            ctf = Convert.ToSingle(Console.ReadLine());
+
+            nf = 0.55f * (cp1 + cp2 + cp3) / 3 + 0.30f * cef + 0.15f * ctf;
+
+            Console.WriteLine("El producto final es: " + nf);
+            Console.ReadKey();
+        }
+    }
+}
